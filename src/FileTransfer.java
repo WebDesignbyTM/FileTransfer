78c6c940c789d68d70ed82e1543b676d53709e93
ioanviorelgg <viorel.ghiorghiu@golfgenius.com>
2021-12-13 13:06:55 +0200

    Set up general todos

--- src/FileTransfer.java
+++ src/FileTransfer.java
@@ -1,4 +1,9 @@
 public class FileTransfer {
+    /*
+    TODO: 1. File model - get and operate on the accessible files
+    TODO: 2. General view - to be drawn
+    TODO: 3. Command controller - handle input and manage file model / general view
+     */
     public static void main(String[] args) {
         System.out.println("TODO: Implement client and server for FTP connection");
     }
